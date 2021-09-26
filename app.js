c2392c9ff81b91767de554c441d8443fa80bf3ca
PiotrSiwek <siwek.piotr@gmail.com>
2021-09-26 23:36:21 +0200

    randomize blue square showup and increase result when clicked

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ const mole  = document.querySelector('.mole')
 const timeLeft = document.querySelectorAll('.time-left')
 const score = document.querySelector('.score')
 
-document.querySelectorAll('#score')
-
-    let result = 0
-
+let result = 0
+let hitPosition
 function randomSquare() {
     squares.forEach(squares => {
         squares.classList.remove('mole')
     })
-    let randomPosition = squares[Math.floor(Math.random()*9)]
-    console.log(randomPosition)
+    let randomSquare = squares[Math.floor(Math.random()*9)]
+    console.log(randomSquare)
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
 }
 
-randomSquare()
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+
+
+
+    })
+})
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+
+}
+
+moveMole()
