b471be96f7c4b32e2c62fd9993125bc51c058961
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-02-20 23:31:27 +0100

    Update start game button styling

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ document.getElementById('startButton').addEventListener('click', startGame);
 
         let result = 0
         let hitPosition
-        let currentTime = 9
+        let currentTime = 10
         let timerId = null
+        let countDownTimerId = null
 
 
         function randomSquare() {
             squares.forEach(squares => {
                 squares.classList.remove('mole')
             })
+
             let randomSquare = squares[Math.floor(Math.random()*9)]
             console.log(randomSquare)
             randomSquare.classList.add('mole')
@@ -26,26 +28,20 @@ document.getElementById('startButton').addEventListener('click', startGame);
             square.addEventListener('mousedown', () => {
                 if (square.id == hitPosition) {
                     result++
-                    score.textContent = result
+                    score.textContent = + result
                     hitPosition = null
                 }
-
-
-
             })
         })
 
         function moveMole() {
-
             timerId = setInterval(randomSquare, 750)
 
         }
-
         moveMole()
-
         function countDown() {
             currentTime--
-            timeLeft.textContent = currentTime
+            timeLeft.textContent = 'Time Left: ' + currentTime
 
             if (currentTime==0) {
                 clearInterval(timerId)
@@ -54,7 +50,8 @@ document.getElementById('startButton').addEventListener('click', startGame);
 
             }
         }
-
-
-        let countDownTimerId = setInterval(countDown, 1000)
+      
+        countDownTimerId = setInterval(countDown, 1000)
     }
+
+
